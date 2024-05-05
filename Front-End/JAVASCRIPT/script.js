8a13eddcfabef63fd91359152cc1db4b72be93fd
joaouzeloto <joaoricardouzelotosilva2003@gmail.com>
2024-05-05 00:12:32 -0300

    -carousel

---
+++ Front-End/JAVASCRIPT/script.js
@@ -0,0 +1,12 @@
+let count = 1;
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){nextImg()}, 2000)
+
+function nextImg(){
+    count++;
+    if(count>4)
+        count = 1;
+    document.getElementById("radio"+count).checked = true;
+}
